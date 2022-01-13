ed1f7026f844e1342bfe71a5f0c068451ed186ac
Michal Stepnik <mstepnik1@nait.ca>
2022-01-12 17:12:30 -0800

    finished demo

--- HelloIOSWorld/HelloIOSWorld/ViewController.swift
+++ HelloIOSWorld/HelloIOSWorld/ViewController.swift
@@ -20,13 +20,28 @@ class ViewController: UIViewController {
     
     //Mark: Custom Actions
     @IBAction func changeGreeting(_ sender: UIButton) {
+        if let name = nameTextField.text {
+            if let age = ageTextField.text{
+                let message = "Hello \(name). You are \(age) years old!"
+                helloLabel.text = message
+            }
+        }
     }
     
+    //Mark: View Events
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func dismissKeyboard(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func dismissNumpad(_ sender: Any) {
+        ageTextField.resignFirstResponder()
+        nameTextField.resignFirstResponder()
+    }
 }
 
